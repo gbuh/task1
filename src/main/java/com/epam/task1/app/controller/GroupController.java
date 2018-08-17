411f891427599e5b444826ca9d9fa8681dff3a34
Igor <igorlipko75@gmail.com>
2018-08-17 23:28:25 +0300

    GroupController edited

--- src/main/java/com/epam/task1/app/controller/GroupController.java
+++ src/main/java/com/epam/task1/app/controller/GroupController.java
@@ -1,18 +1,49 @@
 package com.epam.task1.app.controller;
 
-import com.epam.task1.app.service.UserService;
+import com.epam.task1.app.domain.Group;
+import com.epam.task1.app.service.GroupService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collection;
 
 @RestController
 @RequestMapping("/groups")
 public class GroupController {
 
-    UserService groupService;
+    GroupService groupService;
 
-    public GroupController(UserService groupService) {
+    public GroupController(GroupService groupService) {
         this.groupService = groupService;
     }
 
+    @GetMapping("/{id}")
+    Group group(Long id) {
+        return groupService.getById(id);
+    }
 
+    @GetMapping
+    Collection<Group> groups() {
+        return groupService.getAll();
+    }
+
+    @PostMapping
+    String createGroup(Group group) {
+        groupService.insert(group);
+        return "redirect:/groups";
+    }
+
+    @PostMapping
+    String deleteGroup(Long id) {
+        groupService.delete(id);
+        return "redirect:/groups";
+    }
+
+    @PostMapping
+    String updateGroup(Long id, Group group) {
+        groupService.update(id, group);
+        return "redirect:/groups";
+    }
 }
