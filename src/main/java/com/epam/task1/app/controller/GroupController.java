c7692a0fe21346c9e49579c16f7c20f5221d8553
Igor <igorlipko75@gmail.com>
2018-08-17 23:18:24 +0300

    GroupController edited

--- src/main/java/com/epam/task1/app/controller/GroupController.java
+++ src/main/java/com/epam/task1/app/controller/GroupController.java
@@ -1,6 +1,6 @@
 package com.epam.task1.app.controller;
 
-import com.epam.task1.app.service.GroupService;
+import com.epam.task1.app.service.UserService;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,9 +8,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/groups")
 public class GroupController {
 
-    GroupService groupService;
+    UserService groupService;
 
-    public GroupController(GroupService groupService) {
+    public GroupController(UserService groupService) {
         this.groupService = groupService;
     }
 
