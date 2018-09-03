371ba5fd4ef5ce676eace4e48317c19ead614a75
Igor <igorlipko75@gmail.com>
2018-09-03 16:53:11 +0300

    Print all Spring Boot Beans that loaded in application

--- src/test/java/com/epam/task1/app/ApplicationTest.java
+++ src/test/java/com/epam/task1/app/ApplicationTest.java
@@ -10,7 +10,7 @@ import org.springframework.test.context.junit4.SpringRunner;
  */
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class Task1ApplicationTest {
+public class ApplicationTest {
 
     @Test
     public void contextLoads() {
