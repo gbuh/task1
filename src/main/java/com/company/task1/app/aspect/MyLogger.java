c96c7f254b5eb290961d03b22929deb2ad18be15
Ihar_Lipko <Ihar_Lipko@EPBYVITW0178.minsk.epam.com>
2018-09-06 13:30:08 +0300

    Experimental Aspect MyLogger

--- src/main/java/com/company/task1/app/aspect/MyLogger.java
+++ src/main/java/com/company/task1/app/aspect/MyLogger.java
@@ -1,6 +1,5 @@
 package com.company.task1.app.aspect;
 
-import com.company.task1.app.entity.User;
 import lombok.extern.log4j.Log4j2;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
@@ -8,12 +7,23 @@ import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.context.annotation.Configuration;
 
-import java.util.Arrays;
-
 @Aspect
 @Configuration
 @Log4j2
 public class MyLogger {
+    @Pointcut("within(com.company.task1.app.controller.*)")
+    private void loggingService() {}
+
+    @Around("loggingService()")
+    public Object timeManagement(ProceedingJoinPoint joinPoint) throws Throwable {
+        log.info("Called method : " + joinPoint.getSignature());
+        long startTime = System.currentTimeMillis();
+        Object obj = joinPoint.proceed();
+        long timeTaken = System.currentTimeMillis() - startTime;
+        log.info("Time Taken by {} is {}", joinPoint, timeTaken);
+        return obj;
+    }
+/*
     @Pointcut("@within(org.springframework.web.bind.annotation.RestController)")
     private void loggingService() {}
 
@@ -22,10 +32,11 @@ public class MyLogger {
         log.info("Called method : " + joinPoint.getSignature() + " email" + user.getEmail());
         long startTime = System.currentTimeMillis();
         Object obj = joinPoint.proceed();
-
+        
         Arrays.stream(joinPoint.getArgs()).forEach(arg -> log.info(arg));
         long timeTaken = System.currentTimeMillis() - startTime;
         log.info("Time Taken by {} is {}", joinPoint, timeTaken);
         return obj;
     }
+*/
 }
