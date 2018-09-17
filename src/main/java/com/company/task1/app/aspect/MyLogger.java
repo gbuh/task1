26281a3125495e1e5fa27049a24e3fdd3287572a
Ihar_Lipko <Ihar_Lipko@EPBYVITW0178.minsk.epam.com>
2018-09-17 14:09:14 +0300

    MyLogger edited

--- src/main/java/com/company/task1/app/aspect/MyLogger.java
+++ src/main/java/com/company/task1/app/aspect/MyLogger.java
@@ -24,19 +24,86 @@ public class MyLogger {
         return obj;
     }
 /*
-    @Pointcut("@within(org.springframework.web.bind.annotation.RestController)")
-    private void loggingService() {}
+    @Around(value = "@annotation(logging)")
+    public Object logMethod(final ProceedingJoinPoint joinPoint, final Logging logging) throws Throwable {
+        MethodSignature signature = castToMethodSignature(joinPoint.getSignature());
+        Logger log = LogManager.getLogger(signature.getDeclaringType());
+        String methodDescription = buildMethodDescription(logging.value(),
+                getLoggableParameters(signature, joinPoint.getArgs()));
+        log.debug(String.format("Started. %s.", methodDescription));
+        Object resultObject = joinPoint.proceed();
+        log.debug(String.format("Finished. %s.%s", methodDescription, getResultPart(signature, resultObject)));
+        return resultObject;
+    }
 
-    @Around("loggingService() && args(user)")
-    public Object timeManagement(ProceedingJoinPoint joinPoint, User user) throws Throwable {
-        log.info("Called method : " + joinPoint.getSignature() + " email" + user.getEmail());
-        long startTime = System.currentTimeMillis();
-        Object obj = joinPoint.proceed();
+    private MethodSignature castToMethodSignature(final Signature signature) {
+        if (signature instanceof MethodSignature) {
+            return (MethodSignature) signature;
+        } else {
+            throw new UnsupportedOperationException("Cannot operate on joinpoint signature other than MethodSignature");
+        }
+    }
 
-        Arrays.stream(joinPoint.getArgs()).forEach(arg -> log.info(arg));
-        long timeTaken = System.currentTimeMillis() - startTime;
-        log.info("Time Taken by {} is {}", joinPoint, timeTaken);
-        return obj;
+    private String buildMethodDescription(final String desciption, final List<String> args) {
+        StringBuilder messageBuilder = new StringBuilder(desciption);
+        if (CollectionUtils.isNotEmpty(args)) {
+            return messageBuilder.append(". Parameter(s): ").append(args).toString();
+        }
+        return messageBuilder.toString();
+    }
+
+    private List<String> getLoggableParameters(final MethodSignature methodSignature, final Object[] args) {
+        Parameter[] parameters = methodSignature.getMethod().getParameters();
+        return IntStream.range(0, parameters.length).boxed()
+                .filter(i -> parameters[i].isAnnotationPresent(LogParam.class))
+                .map(i -> String.format("%s=%s", parameters[i].getName(), toString(args[i])))
+                .collect(Collectors.toList());
+    }
+
+    private String getResultPart(final MethodSignature methodSignature, final Object returnedValue) {
+        return isVoidMethod(methodSignature)
+                ? StringUtils.EMPTY
+                : String.format(" Result: %s.", toString(returnedValue));
+    }
+
+    private String toString(final Object object) {
+        if (object == null) {
+            return "null";
+        }
+        if (object instanceof Loggable) {
+            return ((Loggable) object).toLogMessage();
+        }
+        return ToStringBuilder.reflectionToString(object, new LogMessageStyle());
+    }
+
+    private boolean isVoidMethod(final MethodSignature methodSignature) {
+        return methodSignature.getReturnType().equals(void.class);
+    }
+
+    private static final class LogMessageStyle extends RecursiveToStringStyle {
+
+        private static final long serialVersionUID = 1274855978343310410L;
+
+        private LogMessageStyle() {
+            super();
+            super.setUseShortClassName(true);
+            super.setUseIdentityHashCode(false);
+            super.setContentStart("(");
+            super.setContentEnd(")");
+            super.setArrayStart("[");
+            super.setArrayEnd("]");
+            super.setArraySeparator(",");
+            super.setFieldSeparator(",");
+        }
+
+        @Override
+        public void appendDetail(final StringBuffer buffer, final String fieldName, final Object value) {
+            if (value instanceof Loggable) {
+                buffer.append(((Loggable) value).toLogMessage());
+            } else {
+                super.appendDetail(buffer, fieldName, value);
+            }
+        }
     }
 */
 }
